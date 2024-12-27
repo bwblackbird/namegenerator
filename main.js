3b687e20f93f969a785a85166940710cf16187dd
cabbidge <121267327+cabbidge@users.noreply.github.com>
2024-12-26 18:54:33 -0600

    Add name generation functionality with word filtering and length handling

--- main.js
+++ main.js
@@ -46,12 +46,70 @@ function parseCSV(text) {
 };
 
 // Name Generation
+function findLength() {
+    // find the number of words in the name
+    let nameLength = document.getElementById("numWords").value;
+    if (nameLength === "") {
+        nameLength = 2;
+        return nameLength;
+    }
+    else {
+        return nameLength
+    }
+}
+
 function filterWords() {
     // filter words based on their order in the name
+    const allFront = [];
+    const allEnd = [];
+    const anyOrder = [];
+    const invalidWords = [];
+    for (const word in wordsDict) {
+        const order = wordsDict[word][0][1];
+        if (order === 'front') {
+            allFront.push(word); 
+        } else if (order === 'end') {
+            allEnd.push(word);
+        } else if (order === 'either') {
+            anyOrder.push(word);
+            allFront.push(word);
+            allEnd.push(word);
+        }
+        else {
+            invalidWords.push(word);
+        }
+    }
+    return [allFront, allEnd, anyOrder];
 };
 
 function generateNames() {
     // randomly generate names based on user input for the number of words per name
+        // TODO: prohibit the same word from being used multiple times in one name
+        // TODO: generate meanings for each name
+        // TODO: display the generated names in the output container
+    const nameLength = findLength();
+    const [allFront, allEnd, anyOrder] = filterWords();
+    const generatedNames = [];
+    for (let j = 0; j < 10; j++) {
+        let name = '';
+        let randomIndex = Math.floor(Math.random() * allFront.length);
+        name += allFront[randomIndex];
+        if (nameLength === 2) {
+            randomIndex = Math.floor(Math.random() * allEnd.length);
+            name += allEnd[randomIndex];
+            generatedNames.push(name);
+        }
+        else {
+            for (let i = 1; i < nameLength - 1; i++) {
+                randomIndex = Math.floor(Math.random() * anyOrder.length);
+                name += anyOrder[randomIndex];
+            }
+            randomIndex = Math.floor(Math.random() * allEnd.length);
+            name += allEnd[randomIndex];
+            generatedNames.push(name);
+        }
+    }
+    console.log(generatedNames);
 };
 
 // Display Results
